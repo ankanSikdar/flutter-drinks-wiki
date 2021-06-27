deb27c21ba3a0d0192e97c6049b0f71d5073db7a
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-27 21:30:57 +0530

    Added drink list model

---
+++ lib/models/drink_list.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+import 'package:meta/meta.dart';
+
+class DrinkList {
+  final int id;
+  final String name;
+  final String imageUrl;
+  DrinkList({
+    @required this.id,
+    @required this.name,
+    @required this.imageUrl,
+  });
+
+  factory DrinkList.fromMap(Map<String, dynamic> map) {
+    return DrinkList(
+      id: map['idDrink'],
+      name: map['strDrink'],
+      imageUrl: map['strDrinkThumb'],
+    );
+  }
+
+  factory DrinkList.fromJson(String source) =>
+      DrinkList.fromMap(json.decode(source));
+}
