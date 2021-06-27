1a34c882def80f0a306cceae4c984c75349f6d49
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-28 00:39:02 +0530

    Added drink cards

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -1,6 +1,9 @@
+import 'package:drinks_wiki/models/drink_list.dart';
+import 'package:drinks_wiki/repositories/repositories.dart';
+import 'package:drinks_wiki/widgets/drink_card.dart';
 import 'package:flutter/material.dart';
 
-class CategoryPage extends StatelessWidget {
+class CategoryPage extends StatefulWidget {
   final String category;
 
   const CategoryPage({
@@ -9,11 +12,59 @@ class CategoryPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _CategoryPageState createState() => _CategoryPageState();
+}
+
+class _CategoryPageState extends State<CategoryPage> {
+  Future<List<DrinkList>> drinkListFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    drinkListFuture = getDrinksList(widget.category);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(category),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.category),
+        ),
+        body: FutureBuilder(
+          future: drinkListFuture,
+          builder:
+              (BuildContext context, AsyncSnapshot<List<DrinkList>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasData) {
+                return GridView.builder(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 0.78,
+                    crossAxisSpacing: 10.0,
+                    mainAxisSpacing: 7.0,
+                  ),
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (context, index) {
+                    final drink = snapshot.data[index];
+                    return DrinkCard(
+                      key: ValueKey(drink.id),
+                      id: drink.id,
+                      title: drink.name,
+                      imageUrl: drink.imageUrl,
+                    );
+                  },
+                );
+              } else {
+                return Center(
+                  child: Text('No Data'),
+                );
+              }
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ));
   }
 }
---
+++ lib/widgets/drink_card.dart
@@ -0,0 +1,44 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class DrinkCard extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const DrinkCard({
+    Key key,
+    @required this.id,
+    @required this.title,
+    @required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final borderRadius = BorderRadius.circular(15.0);
+    return Column(
+      children: [
+        ClipRRect(
+          borderRadius: borderRadius,
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
+            placeholder: (context, url) {
+              return Center(child: CircularProgressIndicator());
+            },
+          ),
+        ),
+        SizedBox(height: 8.0),
+        Text(
+          title,
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+        ),
+      ],
+    );
+  }
+}
