b442cab49cefc224c67903a1ea4bbc2318315c4f
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-28 21:47:03 +0530

    Added glass type data

---
+++ lib/models/glass_type.dart
@@ -0,0 +1,103 @@
+import 'package:drinks_wiki/widgets/category_card.dart';
+
+List<CategoryCard> glassCategories = [
+  CategoryCard(
+    title: 'Highball glass',
+    imageUrl:
+        'https://images.unsplash.com/photo-1498772776855-2248a3e740f3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+  ),
+  CategoryCard(
+    title: 'Cocktail glass',
+    imageUrl:
+        'https://images.unsplash.com/photo-1559778920-fb8cd0e1ec86?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+  ),
+  CategoryCard(
+    title: 'Whiskey Glass',
+    imageUrl:
+        'https://images.unsplash.com/photo-1615887023544-3a566f29d822?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8',
+  ),
+  CategoryCard(
+    title: 'Collins glass',
+    imageUrl:
+        'https://cdnimg.webstaurantstore.com/images/products/large/198548/1814237.jpg',
+  ),
+  CategoryCard(
+    title: 'Pousse cafe glass',
+    imageUrl: 'https://live.staticflickr.com/3575/3389589263_80beddb772_b.jpg',
+  ),
+  CategoryCard(
+    title: 'Champagne flute',
+    imageUrl:
+        'https://specials-images.forbesimg.com/imageserve/504240401/960x0.jpg?fit=scale',
+  ),
+  CategoryCard(
+    title: 'Whiskey sour glass',
+    imageUrl:
+        'https://robbreport.com/wp-content/uploads/2021/05/whiskey_sour.jpg?w=1000',
+  ),
+  CategoryCard(
+    title: 'Cordial glass',
+    imageUrl:
+        'https://www.etchworld.com/media/cms/articlestash/XmasCordial-Glass500.jpg',
+  ),
+  CategoryCard(
+    title: 'Brandy snifter',
+    imageUrl:
+        'https://www.restaurantware.com/media/catalog/product/R/W/RWG0084-3-LR.jpg?quality=80&fit=bounds&height=700&width=700',
+  ),
+  CategoryCard(
+    title: 'Hurricane glass',
+    imageUrl:
+        'https://upload.wikimedia.org/wikipedia/commons/f/f4/Hurricane_at_Pat_O%27Brien%27s.JPG',
+  ),
+  CategoryCard(
+    title: 'Shot glass',
+    imageUrl:
+        'https://ii1.pepperfry.com/media/catalog/product/o/c/568x625/ocean-plaza-glass-55-ml-shot-glasses---set-of-6-ocean-plaza-glass-55-ml-shot-glasses---set-of-6-v1usci.jpg',
+  ),
+  CategoryCard(
+    title: 'Irish coffee cup',
+    imageUrl:
+        'https://jerseyshorepartyshop.com/wp-content/uploads/2019/06/glassware-irish-coffee-mug.jpg',
+  ),
+  CategoryCard(
+    title: 'Punch bowl',
+    imageUrl:
+        'https://media.istockphoto.com/photos/fruit-punch-bowl-picture-id172736947',
+  ),
+  CategoryCard(
+    title: 'Pint glass',
+    imageUrl:
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSt-K9zMTSVnxjatyPF8qCf-ezqicqxqXz23Q&usqp=CAU',
+  ),
+  CategoryCard(
+    title: 'Wine Glass',
+    imageUrl:
+        'https://post.medicalnewstoday.com/wp-content/uploads/sites/3/2020/02/320404_1100-1100x628.jpg',
+  ),
+  CategoryCard(
+    title: 'Beer pilsner',
+    imageUrl:
+        'https://cdn.shopify.com/s/files/1/0001/8264/8896/products/Marines_pils_1024x1024.jpg?v=1620840453',
+  ),
+  CategoryCard(
+    title: 'Mason jar',
+    imageUrl:
+        'https://i.pinimg.com/originals/fe/49/78/fe497865f0125b0acd80a75a0fc012e3.jpg',
+  ),
+  CategoryCard(
+    title: 'Margarita glass',
+    imageUrl:
+        'https://cdnimg.webstaurantstore.com/images/products/large/121155/1841823.jpg',
+  ),
+  CategoryCard(
+    title: 'Martini Glass',
+    imageUrl:
+        'https://i2.wp.com/www.moodymixologist.com/wp-content/uploads/2020/05/IMG_2870-c-9304565.jpg?fit=682%2C1024&ssl=1',
+  ),
+  CategoryCard(
+    title: 'Coupe Glass',
+    imageUrl:
+        'https://www.homewetbar.com/media/catalog/product/cache/265d7bf611d39b8f80e93d32d7319b33/W/1/W105613-coupe-glass-452921.jpg',
+  ),
+];
