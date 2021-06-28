558c1f197cfd2b805fc9e099f9c07eef28ccf9db
Ankan Sikdar <ankansikdar@gmail.com>
2021-06-28 20:43:13 +0530

    Changed to List View and added Title

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,14 +9,28 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.fromLTRB(16.0, 32.0, 0.0, 32.0),
-        child: CustomScrollView(
-          slivers: [
-            SliverToBoxAdapter(
-              child: SizedBox(
-                height: 400,
-                child: CategoryGridView(),
+        child: ListView(
+          children: [
+            Container(
+              alignment: Alignment.bottomLeft,
+              margin: EdgeInsets.only(bottom: 8.0),
+              child: Text(
+                'Categories',
+                style: TextStyle(fontSize: 42.0),
               ),
-            )
+            ),
+            SizedBox(
+              height: 400,
+              child: CategoryGridView(),
+            ),
+            Container(
+              alignment: Alignment.bottomLeft,
+              margin: EdgeInsets.fromLTRB(0.0, 16.0, 0.0, 8.0),
+              child: Text(
+                'Glass Type',
+                style: TextStyle(fontSize: 42.0),
+              ),
+            ),
           ],
         ),
       ),
