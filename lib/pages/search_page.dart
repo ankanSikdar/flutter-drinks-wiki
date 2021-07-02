e3ac57498e8686196502064e28940d80d59c4cdc
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-02 21:38:10 +0530

    Added appbar to search page

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,14 +1,37 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({Key key}) : super(key: key);
 
   @override
+  _SearchPageState createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController _controller = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('Search Page'),
-      ),
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          centerTitle: true,
+          title: TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              hintText: 'Enter Drink Name',
+              border: InputBorder.none,
+            ),
+          ),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.search_sharp),
+                onPressed: () {
+                  print(_controller.text);
+                }),
+          ],
+        )
+      ],
     );
   }
 }
