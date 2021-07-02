b2ee2a67fdebc4bc1329bee4458d590f0ee6f126
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-02 22:03:25 +0530

    Added search functionality

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -1,4 +1,7 @@
+import 'package:drinks_wiki/models/drink_list.dart';
+import 'package:drinks_wiki/widgets/drinks_grid_view.dart';
 import 'package:flutter/material.dart';
+import 'package:drinks_wiki/repositories/repositories.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key key}) : super(key: key);
@@ -10,27 +13,55 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   final TextEditingController _controller = TextEditingController();
 
+  bool isSearching = false;
+  List<DrinkList> drinkList = [];
+
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
           centerTitle: true,
+          floating: true,
           title: TextField(
             controller: _controller,
             decoration: InputDecoration(
               hintText: 'Enter Drink Name',
               border: InputBorder.none,
             ),
+            keyboardType: TextInputType.name,
+            textAlign: TextAlign.center,
           ),
           actions: [
-            IconButton(
-                icon: Icon(Icons.search_sharp),
-                onPressed: () {
-                  print(_controller.text);
-                }),
+            isSearching
+                ? Padding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
+                    child: CircularProgressIndicator(
+                      strokeWidth: 1.0,
+                    ),
+                  )
+                : IconButton(
+                    icon: Icon(Icons.search_sharp),
+                    onPressed: () async {
+                      if (isSearching)
+                        return;
+                      else
+                        setState(() {
+                          isSearching = true;
+                        });
+                      final name = _controller.text;
+                      drinkList = await getSearchedDrinksList(name: name);
+                      setState(() {
+                        isSearching = false;
+                      });
+                    },
+                  ),
           ],
-        )
+        ),
+        SliverPadding(
+            padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+            sliver: DrinksGridView(drinkList)),
       ],
     );
   }
