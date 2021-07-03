ac93ff4b70c24026eddb2cfdf6c0d1e5c166e05b
Ankan Sikdar <ankansikdar@gmail.com>
2021-07-03 13:59:21 +0530

    Added drink card to favorites page

--- lib/pages/favorites_page.dart
+++ lib/pages/favorites_page.dart
@@ -1,5 +1,6 @@
 import 'package:drinks_wiki/cubit/favorites_cubit.dart';
 import 'package:drinks_wiki/models/drink_model.dart';
+import 'package:drinks_wiki/widgets/drink_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -20,11 +21,40 @@ class _LikesPageState extends State<LikesPage> {
 
   @override
   Widget build(BuildContext context) {
-    drinks = context.read<FavoritesCubit>().state.favorites;
-    return Container(
-      child: Center(
-        child: Text('${drinks.length}'),
-      ),
+    return BlocBuilder<FavoritesCubit, FavoritesState>(
+      builder: (context, state) {
+        drinks = context.read<FavoritesCubit>().state.favorites;
+        return drinks.length == 0
+            ? Container(
+                child: Center(
+                  child: Text('No Favorites Added Yet'),
+                ),
+              )
+            : CustomScrollView(
+                slivers: [
+                  SliverPadding(
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
+                    sliver: SliverList(
+                      delegate: SliverChildBuilderDelegate(
+                        (BuildContext context, int index) {
+                          final drink = drinks[index];
+                          return Padding(
+                            padding: EdgeInsets.symmetric(vertical: 18.0),
+                            child: DrinkCard(
+                              id: drink.id,
+                              title: drink.name,
+                              imageUrl: drink.imageUrl,
+                            ),
+                          );
+                        },
+                        childCount: drinks.length,
+                      ),
+                    ),
+                  )
+                ],
+              );
+      },
     );
   }
 }
